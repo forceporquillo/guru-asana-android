733fdf1f390fda8611e747c8caa8416554fe09f8
strongforce1 <fporquillo18@gmail.com>
2022-11-08 12:59:13 +0800

    Add `:lib-discrete-scrollview` library

---
+++ lib-discrete-scrollview/src/main/java/dev/forcecodes/guru/Direction.java
@@ -0,0 +1,50 @@
+package dev.forcecodes.guru;
+
+/**
+ * Created by yarolegovich on 16.03.2017.
+ */
+enum Direction {
+
+    START {
+        @Override
+        public int applyTo(int delta) {
+            return delta * -1;
+        }
+
+        @Override
+        public boolean sameAs(int direction) {
+            return direction < 0;
+        }
+
+        @Override
+        public Direction reverse() {
+            return Direction.END;
+        }
+    },
+    END {
+        @Override
+        public int applyTo(int delta) {
+            return delta;
+        }
+
+        @Override
+        public boolean sameAs(int direction) {
+            return direction > 0;
+        }
+
+        @Override
+        public Direction reverse() {
+            return Direction.START;
+        }
+    };
+
+    public abstract int applyTo(int delta);
+
+    public abstract boolean sameAs(int direction);
+
+    public abstract Direction reverse();
+
+    public static Direction fromDelta(int delta) {
+        return delta > 0 ? END : START;
+    }
+}
---
+++ lib-discrete-scrollview/src/main/java/dev/forcecodes/guru/transform/DiscreteScrollItemTransformer.java
@@ -0,0 +1,11 @@
+package dev.forcecodes.guru.transform;
+
+import android.view.View;
+
+/**
+ * Created by yarolegovich on 02.03.2017.
+ */
+
+public interface DiscreteScrollItemTransformer {
+    void transformItem(View item, float position);
+}
